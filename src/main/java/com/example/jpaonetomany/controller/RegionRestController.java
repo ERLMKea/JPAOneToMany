f98c1edbcc40c16264fbde253479dea96bd174da
erlm <erlm@kea.dk>
2023-09-18 08:51:37 +0200

    getkommunenavne post og put

--- src/main/java/com/example/jpaonetomany/controller/RegionRestController.java
+++ src/main/java/com/example/jpaonetomany/controller/RegionRestController.java
@@ -6,10 +6,7 @@ import com.example.jpaonetomany.service.ApiServiceGetRegioner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,6 +25,33 @@ public class RegionRestController {
         List<Region> lstRegioner = apiServiceGetRegioner.getRegioner();
         return lstRegioner;
     }
+
+    @PostMapping("/region")
+    public ResponseEntity<Region> postRegion(@RequestBody Region region) {
+        System.out.println("Indsætter ny Region");
+        System.out.println(region);
+        Region savedRegion = regionRepository.save(region);
+        if (savedRegion == null) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        } else {
+            return new ResponseEntity<>(savedRegion, HttpStatus.CREATED);
+        }
+    }
+
+    @PutMapping("/region/{kode}")
+    public ResponseEntity<Region> putRegion(@PathVariable String kode, @RequestBody Region region) {
+        Optional<Region> orgRegion = regionRepository.findById(kode);
+        if (orgRegion.isPresent()) {
+            region.setKode(kode);
+            regionRepository.save(region);
+            return ResponseEntity.ok(region);
+            //return new ResponseEntity<>(student, HttpStatus.OK);
+        } else {
+            return ResponseEntity.notFound().build();
+            //return new ResponseEntity<>(new Student(), HttpStatus.NOT_FOUND);
+        }
+    }
+
 
     @GetMapping("/regioner")
     public List<Region> getRegionerRepos() {
