bbe7b1ab111db30c7fe47c520bb57da3e7d358f4
erlm <erlm@kea.dk>
2023-09-27 11:31:18 +0200

    Paging param

--- src/main/java/com/example/jpaonetomany/controller/KommuneRestController.java
+++ src/main/java/com/example/jpaonetomany/controller/KommuneRestController.java
@@ -6,11 +6,16 @@ import com.example.jpaonetomany.model.Region;
 import com.example.jpaonetomany.repositories.KommuneRepository;
 import com.example.jpaonetomany.service.ApiServiceGetKommuner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -38,6 +43,41 @@ public class KommuneRestController {
     public List<Kommune> getKommuner() {
         return kommuneRepository.findAll();
     }
+
+    @GetMapping("kommunepage")
+    public ResponseEntity<List<Kommune>> getPageOfKommuner() {
+        int page = 4;
+        int size = 5;
+        Pageable paging = PageRequest.of(page, size);
+        Page<Kommune> pageKommune = kommuneRepository.findAll(paging);
+        List<Kommune> lstKommuner = pageKommune.getContent();
+        return new ResponseEntity<>(lstKommuner, HttpStatus.OK);
+    }
+
+    @GetMapping("/kommunepageparm")
+    public ResponseEntity<Map<String, Object>> getPageOfKommuner(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+
+        Pageable paging = PageRequest.of(page, size);
+        Page<Kommune> pageKommune = kommuneRepository.findAll(paging);
+        List<Kommune> kommuner = pageKommune.getContent();
+
+        if (kommuner.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("kommuner", kommuner);
+        response.put("currentPage", pageKommune.getNumber());
+        response.put("totalItems", pageKommune.getTotalElements());
+        response.put("totalPages", pageKommune.getTotalPages());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+
+    }
+
+
+
 
     @PostMapping("/kommune")
     public ResponseEntity<Kommune> postRegion(@RequestBody Kommune kommune) {
